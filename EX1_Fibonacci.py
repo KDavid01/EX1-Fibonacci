0d7a62e8ae2e9005396211648983270d6230e985
KDavid01 <36845104+KDavid01@users.noreply.github.com>
2018-04-30 16:23:52 +0200

    Add files via upload

---
+++ EX1_Fibonacci.py
@@ -0,0 +1,12 @@
+def Fibonacci():
+    elso_szam=1
+    masodik_szam=1
+    print(elso_szam,end=' ')
+    print(masodik_szam,end=' ')
+    for i in range(98):
+        osszeg=elso_szam+masodik_szam
+        print(osszeg,end=' ')
+        elso_szam=masodik_szam
+        masodik_szam=osszeg
+
+Fibonacci()
