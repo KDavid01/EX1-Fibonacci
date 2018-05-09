22cfc4d5a62088a747ee36375ac3b70e43bdf7b7
KDavid01 <36845104+KDavid01@users.noreply.github.com>
2018-05-09 20:20:00 +0200

    Add files via upload

---
+++ EX6_Morse.py
@@ -0,0 +1,43 @@
+morseABC={'A': '.-', 'B': '-...', 'C': '-.-.',
+          'D': '-..', 'E': '.', 'F': '..-.',
+          'G': '--.', 'H': '....', 'I': '..',
+          'J': '.---', 'K': '-.-', 'L': '.-..',
+          'M': '--', 'N': '-.', 'O': '---',
+          'P': '.--.', 'Q': '--.-', 'R': '.-.',
+          'S': '...', 'T': '-', 'U': '..-',
+          'V': '...-', 'W': '.--', 'X': '-..-',
+          'Y': '-.--', 'Z': '--..',
+
+          '0': '-----', '1': '.----', '2': '..---',
+          '3': '...--', '4': '....-', '5': '.....',
+          '6': '-....', '7': '--...', '8': '---..',
+          '9': '----.', '!':'--.--', ',':'--..--',
+          '.':'.-.-.-','?':'..--..', '/':'-..-.',
+          '-':'-....-','(':'-.--.', ')':'-.--.-'}
+
+def forditoMorse(input):
+    tmp=''
+    szoveg=input.upper()
+    for szo in szoveg:
+        for s1,s2 in morseABC.items():
+            if szo == s1:
+                tmp +=s2
+        tmp += ' '
+    print(tmp)
+
+def forditoABC(input2):
+    newTmp = ''
+    for karakter in input2.split(' '):
+        for s1,s2 in morseABC.items():
+            if karakter == s2:
+                newTmp += s1
+        newTmp +=' '
+    print(newTmp)
+
+def main():
+    x=input("Mire forditsak(morze/abc)?: ")
+    if x=='morze':
+        return forditoMorse(input("Irja be a szoveget: "))
+    else:
+        return forditoABC(input("Irja be a morzet: "))
+main()
